703ba02a9f9c74f8c330ceb1500734d3d75e84b0
Himanshu Shekhar <shekharhimanshu679@gmail.com>
2026-09-12 18:22:56 +0530

    date

--- backend/backend/src/main/java/com/shopstack/backend/BackendApplication.java
+++ backend/backend/src/main/java/com/shopstack/backend/BackendApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class BackendApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BackendApplication.class, args);
-	}
+    public static void main(String[] args) {
+        System.out.println("JAVA TIMEZONE = " + java.time.ZoneId.systemDefault());
+        System.out.println("JAVA LOCAL TIME = " + java.time.LocalDateTime.now());
 
+        SpringApplication.run(BackendApplication.class, args);
+    }
 }
