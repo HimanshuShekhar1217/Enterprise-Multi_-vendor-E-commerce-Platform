1a72326a5adcaa385d7d22e9d58d28a0a43d1aa7
Himanshu Shekhar <shekharhimanshu679@gmail.com>
2026-09-13 01:52:56 +0530

    jj

--- backend/backend/src/main/java/com/shopstack/backend/service/EmailService.java
+++ backend/backend/src/main/java/com/shopstack/backend/service/EmailService.java
@@ -1,5 +1,6 @@
 package com.shopstack.backend.service;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -12,11 +13,15 @@ public class EmailService {
 
     private final JavaMailSender mailSender;
 
+    @Value("${MAIL_FROM}")
+    private String fromEmail;
+
     public boolean sendEmail(String to, String subject, String body) {
 
         try {
             SimpleMailMessage message = new SimpleMailMessage();
 
+            message.setFrom(fromEmail);
             message.setTo(to);
             message.setSubject(subject);
             message.setText(body);
